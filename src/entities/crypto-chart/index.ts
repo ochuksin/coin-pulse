1f610e018699b8d953f6c76b8ebf23c1a1c0a097
Oleg Chuksin <ochuksin@gmail.com>
2026-06-11 16:51:41 +0300

    fetch coingecko

---
+++ src/entities/crypto-chart/index.ts
@@ -0,0 +1,2 @@
+export { default as CryptoChart } from "./ui/CryptoChart";
+export type { DataPoint } from "./model/types";
---
+++ src/entities/crypto-chart/model/types.ts
@@ -0,0 +1,4 @@
+export interface DataPoint {
+  date: string;
+  price: number;
+}
